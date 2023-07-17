322c21a68920cace5ad73d4973cea5ce195d34f2
Feyzaan <125843818+Feyzaan@users.noreply.github.com>
2023-07-17 20:10:23 +0530

    Add files via upload

---
+++ Self/ASCII value.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+	
+	int i;
+	char ch;
+	
+	printf("Enter Alphabet to convert into ASCII: ");
+	scanf("%c",&ch);
+	
+	printf("The ASCII value of %c: %d",ch,ch);
+	
+	
+	return 0;
+}
